da7a8419a92ffe16d539b5b00f5703a9f005506b
Shaunak Joshi <shaunakjoshi1407@gmail.com>
2023-04-21 21:01:01 +0530

    Fixing TC builds for k8s-dashboard to not rely on binary
    Adding make targets in makefile

---
+++ install_dependency.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+curl -sL -o ~/.local/bin/gimme https://raw.githubusercontent.com/travis-ci/gimme/master/gimme
+
+chmod +x ~/.local/bin/gimme
+
+eval "$(gimme 1.17)"
+
+# Install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+
+# Load nvm
+source ~/.nvm/nvm.sh
+
+# Install node 16.13.0
+nvm install v16.13.0
+
+
+npm install -g npm@6.14.10
+
+npm --version
+
+node -v
+
+npm ci
+
+npm i bc
+
+export TERM=xterm-256color
+./aio/scripts/build.sh
