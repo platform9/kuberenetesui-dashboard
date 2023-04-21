e39f9288a760efd65b8462a34f6a96bfbffae89f
Shaunak Joshi <shaunakjoshi1407@gmail.com>
2023-04-21 15:48:39 +0530

    Dependency installation

---
+++ install_dependency.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+
+# Load nvm
+source ~/.nvm/nvm.sh
+
+# Install node 16.13.0
+nvm install v16.13.0
+
+nvm --version
+
+npm install -g npm@6.14.10
+
+npm --version
+
+node -v
+
+npm ci
+
+npm i bc
+
+./aio/scripts/build.sh
