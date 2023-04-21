f49dde2b104eec4daacfdb03594b52b4f530293a
Rohitrajak1807 <rrajak@platform9.com>
2023-04-21 12:24:42 +0000

    syntax fix

--- install_dependency.sh
+++ install_dependency.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+curl -sL -o ~/.local/bin/gimme https://raw.githubusercontent.com/travis-ci/gimme/master/gimme
+
+chmod +x ~/.local/bin/gimme
+
+eval "$(gimme 1.17)"
+
 # Install nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
 
@@ -9,7 +15,6 @@ source ~/.nvm/nvm.sh
 # Install node 16.13.0
 nvm install v16.13.0
 
-nvm --version
 
 npm install -g npm@6.14.10
 
